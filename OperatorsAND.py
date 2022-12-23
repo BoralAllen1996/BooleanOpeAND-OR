5e1670b3ce0d822603d519cb9c0cb74bb070beea
lenbo <allenbora@gmail.com>
2022-12-24 01:51:50 +0800

    Seventeenth

---
+++ OperatorsAND.py
@@ -0,0 +1,14 @@
+#False
+statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+print(statement_one)
+#True
+statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
+print(statement_two)
+
+credits = 120
+gpa = 3.4
+
+if gpa >= 2.0 and credits >= 120:
+  print("You meet the requirements to graduate!")
+
+#end....
---
+++ OperatorsOR.py
@@ -0,0 +1,15 @@
+#True
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
+print(statement_one)
+
+#True
+statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
+print(statement_two)
+
+credits = 118
+gpa = 2.0
+
+if gpa >= 2.0 or credits >= 120:
+  print("You have met at least one of the requirements.")
+
+#End......
